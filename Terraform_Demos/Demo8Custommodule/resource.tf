30681bad6c49ef79b681cca1ac54d5a52180ea44
Arfa7007 <90682541+Arfa7007@users.noreply.github.com>
2022-03-01 16:07:37 +0530

    Delete Terraform_Demos/Demo8Custommodule directory

--- Terraform_Demos/Demo8Custommodule/resource.tf
+++
@@ -1,9 +0,0 @@
-module "s3bucket" {
-  source = "./modules/s3bucket"
-  bucket = "terraform-learning-2022-feb-28"
-}
-
-output "bucketarn" {
-  value = module.s3bucket.bucketarn
-  description = "arn number of the bucket"
-}
